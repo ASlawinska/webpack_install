de1392ddc9cd0159ff3b0034a58504529c22ff6e
ASlawinska <slawinska.adrianna@gmail.com>
2021-02-11 14:16:39 +0100

    Added image file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import style from "./css/index.scss";
+import Icon from "./../assets/img/proba.png"
+let myIcon = new Image();
+myIcon.src = Icon;
+document.querySelector("div").appendChild(myIcon);
+document.querySelector("div").classList.add("change");
 
 console.log("Hello world");
