72c51f639b96dbef188d9404dbed9de8bd980b38
ASlawinska <slawinska.adrianna@gmail.com>
2021-02-11 12:24:18 +0100

    Added automatically refreshed

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,10 @@ entry: "./src/index.js",
 output: {
 path: path.resolve(__dirname, "dist"),
 filename: "bundle.js"
-}
+},
+devServer:{
+    contentBase: path.join(__dirname,"dist"),
+    port: 9000,
+    watchContentBase: true
+    }
 }
